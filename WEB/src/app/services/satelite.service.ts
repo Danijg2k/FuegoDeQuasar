8a7a3300af58486bef4313c3dc824184df1b60dc
Danijg2k <danijg2000@gmail.com>
2022-07-27 08:44:39 +0200

    Función Post Split también utiliza Axios.

--- WEB/src/app/services/satelite.service.ts
+++ WEB/src/app/services/satelite.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { FormGroup } from '@angular/forms';
 import { Satellite } from '../models/enviar/satellite.model';
+import axios from 'axios';
 
 @Injectable()
 export class SateliteService {
@@ -22,4 +23,25 @@ export class SateliteService {
       observe: 'response',
     });
   }
+
+
+
+  // Enviar satélites de uno en uno - post split (Axios)
+  postSplitAxios(body:FormGroup<any>) {
+    let bodyData = new Satellite();
+    bodyData.name = body.controls['Nombre1'].value;
+    bodyData.distance = body.controls['Distancia1'].value;
+    // Split message into array
+    bodyData.message = body.controls['Mensaje1'].value.split(' ');
+
+    return axios.post(environment.API_URL + 'topsecret_split/' + bodyData.name, bodyData).then(res => {
+      return(res.data);
+    }).catch((error) => {
+      console.log(error)
+    });
+  }
+
+
 }
+
+
